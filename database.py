93882877ee67a156db41aa2d2bec60ead237a567
ak67civil <taufeeque671@gmail.com>
2026-03-14 00:50:46 +0530

    Update database schema with new tables
    Added new tables for clients, students, and analytics in the database.

--- database.py
+++ database.py
@@ -1,18 +1,37 @@
 import sqlite3
 
-conn = sqlite3.connect("bot.db")
+conn = sqlite3.connect("bot.db", check_same_thread=False)
 cursor = conn.cursor()
 
+# Clients table
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS users(
-user_id INTEGER PRIMARY KEY
+CREATE TABLE IF NOT EXISTS clients(
+client_id INTEGER PRIMARY KEY,
+channel_id INTEGER
 )
 """)
 
+# Students table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS students(
+user_id INTEGER,
+client_id INTEGER
+)
+""")
+
+# Videos table
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS videos(
-file_id TEXT PRIMARY KEY,
-title TEXT
+file_id TEXT,
+client_id INTEGER
+)
+""")
+
+# Analytics table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS analytics(
+user_id INTEGER,
+file_id TEXT
 )
 """)
 
