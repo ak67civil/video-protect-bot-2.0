59e85d8a85f4e040eff8204feb276e2cda8a7c8e
ak67civil <taufeeque671@gmail.com>
2026-03-14 00:40:46 +0530

    Add configuration for bot environment variables

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+
+UPLOAD_CHANNEL = os.getenv("UPLOAD_CHANNEL")
+INDEX_CHANNEL = os.getenv("INDEX_CHANNEL")
+
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+LOG_CHANNEL = os.getenv("LOG_CHANNEL")
