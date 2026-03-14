22c5969339601f981ea8335a0118777c5a7fb7e0
ak67civil <taufeeque671@gmail.com>
2026-03-14 14:58:27 +0530

    Reorder ADMIN_ID assignment in config.py

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ API_HASH = os.getenv("API_HASH")
 
 UPLOAD_CHANNEL = int(os.getenv("UPLOAD_CHANNEL", 0))
 INDEX_CHANNEL = int(os.getenv("INDEX_CHANNEL", 0))
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", 0))
 
 ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
-
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", 0))
