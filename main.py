9efcd37f3bc174c66eef0cd00a04e60734079d5d
ak67civil <taufeeque671@gmail.com>
2026-03-14 17:46:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import asyncio
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import PeerIdInvalid
+
 from config import BOT_TOKEN, API_ID, API_HASH, UPLOAD_CHANNEL, INDEX_CHANNEL, LOG_CHANNEL
 
 bot = Client(
@@ -8,28 +12,44 @@ bot = Client(
     bot_token=BOT_TOKEN
 )
 
+
 @bot.on_message(filters.command("start"))
 async def start(client, message):
     await message.reply_text("🤖 Bot Working!")
+
 
 @bot.on_message(filters.chat(UPLOAD_CHANNEL) & filters.video)
 async def upload_video(client, message):
 
     caption = message.caption if message.caption else "🎬 New Video"
 
-    sent = await client.send_video(
-        chat_id=INDEX_CHANNEL,
-        video=message.video.file_id,
-        caption=caption,
-        protect_content=True
-    )
+    try:
 
-    await client.forward_messages(
-        LOG_CHANNEL,
-        UPLOAD_CHANNEL,
-        message.id
-    )
+        # send video to index channel
+        sent = await client.send_video(
+            chat_id=INDEX_CHANNEL,
+            video=message.video.file_id,
+            caption=caption,
+            protect_content=True
+        )
 
-print("🚀 Bot Started")
+        # forward to log channel
+        await client.forward_messages(
+            chat_id=LOG_CHANNEL,
+            from_chat_id=UPLOAD_CHANNEL,
+            message_ids=message.id
+        )
 
-bot.run()
+        print("✅ Video Indexed")
+
+    except PeerIdInvalid:
+        print("❌ Channel ID invalid or bot not admin")
+
+
+async def main():
+    await bot.start()
+    print("🚀 Bot Started")
+    await asyncio.Event().wait()
+
+
+asyncio.run(main())
