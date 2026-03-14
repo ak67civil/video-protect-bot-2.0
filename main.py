f40301a8ae5afa75baaa793b22241a90abf349a9
ak67civil <taufeeque671@gmail.com>
2026-03-14 15:00:40 +0530

    Refactor bot initialization and clean imports
    Updated bot name and removed unused imports.

--- main.py
+++ main.py
@@ -4,16 +4,12 @@ from config import API_ID, API_HASH, BOT_TOKEN
 import modules.start
 import modules.admin
 import modules.video
-import modules.watch
-import modules.broadcast
 
 app = Client(
-    "video-protect-bot",
+    "coursebot",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN
 )
-
-print("Bot Started Successfully")
 
 app.run()
