4df2423aac8338cc9904ea24fe51800b91930232
ak67civil <taufeeque671@gmail.com>
2026-03-14 14:15:40 +0530

    Convert environment variables to integers with defaults

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_ID = os.getenv("API_ID")
+
+API_ID = int(os.getenv("API_ID", 0))
 API_HASH = os.getenv("API_HASH")
 
-UPLOAD_CHANNEL = os.getenv("UPLOAD_CHANNEL")
-INDEX_CHANNEL = os.getenv("INDEX_CHANNEL")
+UPLOAD_CHANNEL = int(os.getenv("UPLOAD_CHANNEL", 0))
+INDEX_CHANNEL = int(os.getenv("INDEX_CHANNEL", 0))
 
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
-LOG_CHANNEL = os.getenv("LOG_CHANNEL")
+ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
+
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", 0))
