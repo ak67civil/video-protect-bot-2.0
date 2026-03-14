0e4be7f92750884e24bf0c33f4754bfcd929c9c1
ak67civil <taufeeque671@gmail.com>
2026-03-14 14:26:34 +0530

    Refactor bot initialization and import modules

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from pyrogram import Client
-from config import *
+from config import API_ID, API_HASH, BOT_TOKEN
 
 import modules.start
 import modules.admin
 import modules.video
-
+import modules.watch
+import modules.broadcast
 
 app = Client(
-    "course_bot",
-    bot_token=BOT_TOKEN,
+    "video-protect-bot",
     api_id=API_ID,
-    api_hash=API_HASH
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
 )
 
+print("Bot Started Successfully")
 
-if __name__ == "__main__":
-    print("🚀 Bot Started Successfully")
-    app.run()
+app.run()
